e1e65594b82e9449005e56121f3ffba864262d95
reytiartur <reyti.artur@gmail.com>
2023-03-23 12:09:50 +0100

    Scrolling to top on category change and To Top Arrow. Fixed Navbar and Filters.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import './index.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainScreen from './screens/MainScreen';
 
 function App() {
 
   return (
     <div className="App min-h-screen">
-      <Router>
-        <Routes>
-              <Route index element={<MainScreen />} /> 
-        </Routes>
-      </Router>
+      <MainScreen />
     </div>
   )
 }
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,13 +1,15 @@
 import Filters from '../components/Filters'
 import Navbar from '../components/Navbar'
+import ScrollArrow from '../components/ScrollArrow'
 import CompaniesContainer from './CompaniesContainer'
 
 const MainScreen = () => {
   return (
-    <div className='w-full overflow-hidden'>
+    <div className='w-full overflow-hidden relative pt-36'>
       <Navbar />
       <Filters />
       <CompaniesContainer />
+      <ScrollArrow />
     </div>
   )
 }
